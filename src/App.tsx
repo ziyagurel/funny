7e0802044dd043d4db0c21b41d61b25f25a35349
ziyagurel <ziyagurel55@hotmail.com>
2021-04-20 02:59:08 +0300

    preparing style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Wrapper} from "./component/styled/index";
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <Wrapper>
+        <h1>App</h1>
+      </Wrapper>
     </div>
   )
 }
